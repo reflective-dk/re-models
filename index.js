db29b98f7066a9869359d9b45520298fbfeda7b0
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2017-11-10 09:46:19 +0100

    add core module object to bootstrap

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var path = require('path');
 var requireyml = require('require-yml');
 var defloc = path.join(path.dirname(require.resolve('./index')), 'definitions');
 
+var moduleUrn = 'module:core';
 var urns = [
     'class:core:object',
     'class:core:module',
@@ -13,23 +14,23 @@ var urns = [
 
 module.exports = Bootstrap;
 
-function Bootstrap(moduleId) {
-    this.moduleId = moduleId;
+function Bootstrap() {
 }
 
-Bootstrap.prototype.objects = function() {
+Bootstrap.prototype.module = function() {
+    return loadDef(moduleUrn);
+};
+
+Bootstrap.prototype.objects = function(moduleId) {
     var map = {};
-    var moduleId = this.moduleId;
     urns.forEach(function(urn) { map[urn] = loadDef(urn, moduleId); });
     return map;
 };
 
 function loadDef(urn, moduleId) {
     var obj = requireyml(path.join(defloc, urn.replace(/:/g, '-')));
-    obj.registrations[0].validity[0].input.module = { id: moduleId };
+    if (moduleId) {
+        obj.registrations[0].validity[0].input.module = { id: moduleId };
+    }
     return obj;
-}
-
-function clone(o) {
-    return JSON.parse(JSON.stringify(o));
 }
--- test/load-def-test.js
+++ test/load-def-test.js
@@ -8,16 +8,22 @@ var Bootstrap = require.main.require('index.js');
 
 describe('Test definition loading', function() {
     var moduleId = uuid.v4();
-    var bootstrap = new Bootstrap(moduleId);
+    var bootstrap = new Bootstrap();
+    it('should load module:core', function() {
+        bootstrap.module().should.nested.deep.include({
+            'registrations[0].validity[0].input.class': 'class:core:module',
+            'registrations[0].validity[0].input.moduleId': 'module:core'
+        });
+    });
     it('should load class:core:object', function() {
-        bootstrap.objects()['class:core:object'].should.nested.deep.include({
+        bootstrap.objects(moduleId)['class:core:object'].should.nested.deep.include({
             'registrations[0].validity[0].input.module': { id: moduleId },
             'registrations[0].validity[0].input.class': 'class:core:class',
             'registrations[0].validity[0].input.classId': 'class:core:object'
         });
     });
     it('should load class:core:module', function() {
-        bootstrap.objects()['class:core:module'].should.nested.deep.include({
+        bootstrap.objects(moduleId)['class:core:module'].should.nested.deep.include({
             'registrations[0].validity[0].input.module': { id: moduleId },
             'registrations[0].validity[0].input.class': 'class:core:class',
             'registrations[0].validity[0].input.classId': 'class:core:module',
@@ -25,7 +31,7 @@ describe('Test definition loading', function() {
         });
     });
     it('should load class:core:class', function() {
-        bootstrap.objects()['class:core:class'].should.nested.deep.include({
+        bootstrap.objects(moduleId)['class:core:class'].should.nested.deep.include({
             'registrations[0].validity[0].input.module': { id: moduleId },
             'registrations[0].validity[0].input.class': 'class:core:class',
             'registrations[0].validity[0].input.classId': 'class:core:class',
@@ -33,7 +39,7 @@ describe('Test definition loading', function() {
         });
     });
     it('should load class:core:schema-validation', function() {
-        bootstrap.objects()['class:core:schema-validation'].should.nested.deep.include({
+        bootstrap.objects(moduleId)['class:core:schema-validation'].should.nested.deep.include({
             'registrations[0].validity[0].input.module': { id: moduleId },
             'registrations[0].validity[0].input.class': 'class:core:class',
             'registrations[0].validity[0].input.classId': 'class:core:schema-validation',
@@ -41,8 +47,8 @@ describe('Test definition loading', function() {
         });
     });
     it('should reload definitions on every instantiation', function() {
-        bootstrap.objects()['class:core:class'].extraneous = true;
-        bootstrap.objects()['class:core:class'].should.not.include({
+        bootstrap.objects(moduleId)['class:core:class'].extraneous = true;
+        bootstrap.objects(moduleId)['class:core:class'].should.not.include({
             extraneous: true
         });
     });
