3f12483a3a548c257c7a10ff9c2c54b43ad1b6a6
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2017-11-20 13:46:57 +0100

    simplify loading metamodel as dependency

--- index.js
+++ index.js
@@ -4,32 +4,25 @@ var path = require('path');
 var requireyml = require('require-yml');
 var defloc = path.join(path.dirname(require.resolve('./index')), 'definitions');
 
-var modelName = 'model-re-metamodel';
-var names = [
-    'class-re-object',
-    'class-re-model',
-    'class-re-class',
-    'class-re-rule',
-    'class-re-activation',
-    'class-re-validation-result',
-    'rule-model-must-exist',
-    'activation-model-must-exist'
-];
-
 module.exports = Metamodel;
 
 function Metamodel() {
+    this.model = loadDef('model-re-metamodel');
+    this.classes = {
+        model: loadDef('class-re-model'),
+        object: loadDef('class-re-object'),
+        class: loadDef('class-re-class'),
+        rule: loadDef('class-re-rule'),
+        activation: loadDef('class-re-activation'),
+        validationResult: loadDef('class-re-validation-result')
+    };
+    this.rules = {
+        modelMustExist: loadDef('rule-model-must-exist')
+    };
+    this.activations = {
+        modelMustExist: loadDef('activation-model-must-exist')
+    };
 }
-
-Metamodel.prototype.model = function() {
-    return loadDef(modelName);
-};
-
-Metamodel.prototype.objects = function() {
-    var map = {};
-    names.forEach(function(name) { map[name] = loadDef(name); });
-    return map;
-};
 
 function loadDef(name) {
     var obj = requireyml(path.join(defloc, name));
--- test/load-def-test.js
+++ test/load-def-test.js
@@ -8,79 +8,79 @@ var Metamodel = require.main.require('index.js');
 describe('Test definition loading', function() {
     var metamodel = new Metamodel();
     it('should load model ReMetamodel', function() {
-        metamodel.model().should.nested.deep.include({
-            'registrations[0].validity[0].input.class.id': '54985140-62ec-49fd-b05a-661e090c405f',
+        metamodel.model.should.nested.deep.include({
+            'registrations[0].validity[0].input.class.id': metamodel.classes.model.id,
             'registrations[0].validity[0].input.class.name': 'ReModel'
         });
     });
-    it('should load class ReReObject', function() {
-        metamodel.objects()['class-re-object'].should.nested.deep.include({
-            'registrations[0].validity[0].input.class.id': '12b4049a-fb65-4429-a9d7-c91d88a58ac9',
+    it('should load class ReObject', function() {
+        metamodel.classes.object.should.nested.deep.include({
+            'registrations[0].validity[0].input.class.id': metamodel.classes.class.id,
             'registrations[0].validity[0].input.class.name': 'ReClass',
             'registrations[0].validity[0].input.name': 'ReObject'
         });
     });
-    it('should load class ReReModel', function() {
-        metamodel.objects()['class-re-model'].should.nested.deep.include({
-            'registrations[0].validity[0].input.class.id': '12b4049a-fb65-4429-a9d7-c91d88a58ac9',
+    it('should load class ReModel', function() {
+        metamodel.classes.model.should.nested.deep.include({
+            'registrations[0].validity[0].input.class.id': metamodel.classes.class.id,
             'registrations[0].validity[0].input.class.name': 'ReClass',
-            'registrations[0].validity[0].input.extends.id': 'c4315fa9-3765-40c0-ba45-ef821c416f83',
+            'registrations[0].validity[0].input.extends.id': metamodel.classes.object.id,
             'registrations[0].validity[0].input.extends.name': 'ReObject',
             'registrations[0].validity[0].input.name': 'ReModel'
         });
     });
     it('should load class ReClass', function() {
-        metamodel.objects()['class-re-class'].should.nested.deep.include({
-            'registrations[0].validity[0].input.class.id': '12b4049a-fb65-4429-a9d7-c91d88a58ac9',
+        metamodel.classes.class.should.nested.deep.include({
+            'registrations[0].validity[0].input.class.id': metamodel.classes.class.id,
             'registrations[0].validity[0].input.class.name': 'ReClass',
-            'registrations[0].validity[0].input.extends.id': 'c4315fa9-3765-40c0-ba45-ef821c416f83',
+            'registrations[0].validity[0].input.extends.id': metamodel.classes.object.id,
             'registrations[0].validity[0].input.extends.name': 'ReObject',
             'registrations[0].validity[0].input.name': 'ReClass'
         });
     });
-    it('should load class ReReRule', function() {
-        metamodel.objects()['class-re-rule'].should.nested.deep.include({
-            'registrations[0].validity[0].input.class.id': '12b4049a-fb65-4429-a9d7-c91d88a58ac9',
+    it('should load class ReRule', function() {
+        metamodel.classes.rule.should.nested.deep.include({
+            'registrations[0].validity[0].input.class.id': metamodel.classes.class.id,
             'registrations[0].validity[0].input.class.name': 'ReClass',
-            'registrations[0].validity[0].input.extends.id': 'c4315fa9-3765-40c0-ba45-ef821c416f83',
+            'registrations[0].validity[0].input.extends.id': metamodel.classes.object.id,
             'registrations[0].validity[0].input.extends.name': 'ReObject',
             'registrations[0].validity[0].input.name': 'ReRule'
         });
     });
     it('should load class ReActivation', function() {
-        metamodel.objects()['class-re-activation'].should.nested.deep.include({
-            'registrations[0].validity[0].input.class.id': '12b4049a-fb65-4429-a9d7-c91d88a58ac9',
+        metamodel.classes.activation.should.nested.deep.include({
+            'registrations[0].validity[0].input.class.id': metamodel.classes.class.id,
             'registrations[0].validity[0].input.class.name': 'ReClass',
-            'registrations[0].validity[0].input.extends.id': 'c4315fa9-3765-40c0-ba45-ef821c416f83',
+            'registrations[0].validity[0].input.extends.id': metamodel.classes.object.id,
             'registrations[0].validity[0].input.extends.name': 'ReObject',
             'registrations[0].validity[0].input.name': 'ReActivation'
         });
     });
-    it('should load class ReReValidationResult', function() {
-        metamodel.objects()['class-re-validation-result'].should.nested.deep.include({
-            'registrations[0].validity[0].input.class.id': '12b4049a-fb65-4429-a9d7-c91d88a58ac9',
+    it('should load class ReValidationResult', function() {
+        metamodel.classes.validationResult.should.nested.deep.include({
+            'registrations[0].validity[0].input.class.id': metamodel.classes.class.id,
             'registrations[0].validity[0].input.class.name': 'ReClass',
-            'registrations[0].validity[0].input.extends.id': 'c4315fa9-3765-40c0-ba45-ef821c416f83',
+            'registrations[0].validity[0].input.extends.id': metamodel.classes.object.id,
             'registrations[0].validity[0].input.extends.name': 'ReObject',
             'registrations[0].validity[0].input.name': 'ReValidationResult'
         });
     });
     it('should load rule ModelMustExist', function() {
-        metamodel.objects()['rule-model-must-exist'].should.nested.deep.include({
-            'registrations[0].validity[0].input.class.id': '69c26015-0e44-4315-a5d9-343a5308dfef',
+        metamodel.rules.modelMustExist.should.nested.deep.include({
+            'registrations[0].validity[0].input.class.id': metamodel.classes.rule.id,
             'registrations[0].validity[0].input.class.name': 'ReRule',
             'registrations[0].validity[0].input.name': 'ModelMustExist'
         });
     });
     it('should load activation of rule ModelMustExist', function() {
-        metamodel.objects()['activation-model-must-exist'].should.nested.deep.include({
-            'registrations[0].validity[0].input.class.id': 'cb70fe97-13bd-4471-8f5f-a050c7f463bb',
+        metamodel.activations.modelMustExist.should.nested.deep.include({
+            'registrations[0].validity[0].input.class.id': metamodel.classes.activation.id,
             'registrations[0].validity[0].input.class.name': 'ReActivation'
         });
     });
     it('should reload definitions on every instantiation', function() {
-        metamodel.objects()['class-re-class'].extraneous = true;
-        metamodel.objects()['class-re-class'].should.not.include({
+        new Metamodel().classes.class.extraneous = true;
+        new Metamodel().classes.class.should.not.include({
             extraneous: true
         });
     });
