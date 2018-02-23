d6a125221fcd063b254140bfad3087b682eef6af
Kenneth Holm Seelig <kenneth@holmseelig.dk>
2018-02-23 13:57:05 +0000

    added generator

--- scripts/build-objects.js
+++ scripts/build-objects.js
@@ -5,21 +5,8 @@
 var fs = require('fs');
 var path = require('path');
 
-var models = require('../index');
-var objects = [];
-Object.keys(models).forEach(function(m) {
-    var model = models[m];
-    var classes = model.classes || {};
-    var instances = model.instances || {};
-    Object.keys(classes).forEach(function(c) {
-        objects.push(classes[c]);
-    });
-    Object.keys(instances).forEach(function(c) {
-        var collection = instances[c];
-        Object.keys(collection).forEach(function(i) {
-            objects.push(collection[i]);
-        });
-    });
+var generator = require('./generator');
+return generator().then((objects) => {
+  var output = JSON.stringify({ objects: objects }, null, 2);
+  fs.writeFileSync(path.join('build', 'objects.json'), output);
 });
-var output = JSON.stringify({ objects: objects }, null, 2);
-fs.writeFileSync(path.join('build', 'objects.json'), output);
---
+++ scripts/generator.js
@@ -0,0 +1,20 @@
+var models = require(__dirname + '/../index');
+
+module.exports = function generator () {
+  var objects = [];
+  Object.keys(models).forEach(function(m) {
+      var model = models[m];
+      var classes = model.classes || {};
+      var instances = model.instances || {};
+      Object.keys(classes).forEach(function(c) {
+          objects.push(classes[c]);
+      });
+      Object.keys(instances).forEach(function(c) {
+          var collection = instances[c];
+          Object.keys(collection).forEach(function(i) {
+              objects.push(collection[i]);
+          });
+      });
+  });
+  return Promise.resolve(objects);
+};
