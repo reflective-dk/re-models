612d89d15b38769ae43127b4c45b14fec8e21370
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2018-03-07 15:42:20 +0100

    new conf dependency structure

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ var requireyml = require('require-yml');
 var loc = path.join(path.dirname(require.resolve('./index.js')), 'models');
 
 module.exports = requireyml(loc);
+module.exports.state = {
+    model: buildState(module.exports)
+};
+
+// TODO: Move to re-conf-util
+function buildState(conf) {
+    var objects = [];
+    Object.keys(conf).forEach(function(model) {
+        var cnf = conf[model];
+        var classes = cnf.classes || {};
+        var instances = cnf.instances || {};
+        Object.keys(classes).forEach(function(c) {
+            objects.push(classes[c]);
+        });
+        Object.keys(instances).forEach(function(c) {
+            var collection = instances[c];
+            Object.keys(collection).forEach(function(i) {
+                objects.push(collection[i]);
+            });
+        });
+    });
+    return objects;
+}
--- scripts/build-objects.js
+++ scripts/build-objects.js
@@ -4,9 +4,7 @@
 
 var fs = require('fs');
 var path = require('path');
+var models = require('../index');
 
-var generator = require('./generator');
-return generator().then((objects) => {
-  var output = JSON.stringify({ objects: objects }, null, 2);
-  fs.writeFileSync(path.join('build', 'objects.json'), output);
-});
+var output = JSON.stringify({ objects: models.state.model }, null, 2);
+fs.writeFileSync(path.join('build', 'objects.json'), output);
