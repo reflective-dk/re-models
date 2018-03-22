2f77e05210b5a95a69aa8ec29a53596040ef94fb
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2018-03-22 12:04:53 +0100

    report duplicate ids

--- scripts/build-objects.js
+++ scripts/build-objects.js
@@ -2,13 +2,20 @@
 
 "use strict";
 
+var inspect = require('util').inspect;
 var fs = require('fs');
 var path = require('path');
 var confUtil = require('re-conf-util');
 
 var conf = require('../index');
 
-confUtil.buildObjects(conf).then(function(objects) {
-    var output = JSON.stringify({ objects: objects }, null, 2);
-    fs.writeFileSync(path.join('build', 'objects.json'), output);
-});
+confUtil.buildObjects(conf)
+    .then(function(objects) {
+        var output = JSON.stringify({ objects: objects }, null, 2);
+        try { fs.mkdirSync('build'); } catch(e) {}
+        fs.writeFileSync(path.join('build', 'objects.json'), output);
+    })
+    .catch(function(errors) {
+        console.log('Operation failed:');
+        console.log(inspect(errors, null, null));
+    });
